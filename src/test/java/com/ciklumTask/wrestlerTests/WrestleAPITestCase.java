7367e716a49488500a2e1614b7fbe8f02c5a471f
Artem Maliuga <amwyomg@gmail.com>
2018-05-17 19:57:25 +0300

    optimezed test for wrestler creation via API

--- src/test/java/com/ciklumTask/wrestlerTests/WrestleAPITestCase.java
+++ src/test/java/com/ciklumTask/wrestlerTests/WrestleAPITestCase.java
@@ -9,22 +9,23 @@ public class WrestleAPITestCase extends BaseTest {
 
     @Test
     public void testCreateNewWrestlerViaAPI() {
+        String dateOfBirth = "25-05-1994";
         WrestlerModel testWrestler = new WrestlerModel(
-                "aaaaaaa",
-                "6",
-                "3",
-                "2",
-                "3",
-                "2015",
-                "2",
-                "25-05-1994",
-                "1",
-                "2",
-                "2",
-                "3"
-        );
+                faker.name().firstName(),
+                String.valueOf(faker.number().numberBetween(1, 20)),
+                String.valueOf(faker.number().numberBetween(1, 20)),
+                String.valueOf(faker.number().numberBetween(2013, 2017)),
+                faker.name().lastName(),
+                faker.name().lastName(),
+                String.valueOf(faker.number().numberBetween(1, 3)),
+                dateOfBirth,
+                String.valueOf(faker.number().numberBetween(1, 3)),
+                String.valueOf(faker.number().numberBetween(1, 3)),
+                String.valueOf(faker.number().numberBetween(1, 3)),
+                String.valueOf(faker.number().numberBetween(1, 3)));
         WrestlerModel wrestlerResponse = new WrestlerController(testWrestler).createNewWrestler();
-        softAssert.assertTrue(wrestlerResponse.getResult(), "Wrestler is not created!");
+        softAssert.assertTrue(wrestlerResponse.getResult(), "Wrestler is not created! > Result: False");
+        softAssert.assertNotNull(wrestlerResponse.getIdWrestler(), "Wrestler ID is Null");
         softAssert.assertAll();
     }
 }
